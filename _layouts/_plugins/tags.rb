d005c922ad98f54fa1f829b1b9ae6f99ed8aae1c
Dale Emery <dale@dhemery.com>
2019-02-02 16:58:08 -0800

    Tags page and tag pages work

--- _layouts/_plugins/tags.rb
+++ _layouts/_plugins/tags.rb
@@ -1,19 +1,18 @@
 module Jekyll
+
   class TagPage < Page
     def initialize(site:, tag:, posts:)
       @site = site
       @base = site.source
       @dir = 'tag'
-      @slug = Utils.slugify(tag)
+      @name = Utils::slugify(tag)
 
-      process(@slug + '.html')
-      read_yaml(File.join(@base, '_layouts'), 'tag.html')
-      data['title'] = tag.split
-                          .map {|word| word =~ /[[:upper:]].*/ ? word : word.capitalize}
-                          .join(' ')
-      data['posts'] = posts
-      data['tag_page'] = true
-      data['permalink'] = '/:path/:basename/'
+      self.process(@name)
+      self.read_yaml(File.join(@base, '_layouts'), 'tag.html')
+      self.data['title'] = tag
+      self.data['posts'] = posts
+      self.data['tag_page'] = true
+      self.data['permalink'] = '/:path/:basename/'
     end
   end
 
@@ -30,7 +29,7 @@ module Jekyll
         end
       end
       tag_pages = posts_by_tag.map do |tag, posts|
-        TagPage.new(site: site, tag: tag, posts: posts)
+        TagPage.new(site: site, tag: tag, posts: posts )
       end
       site.pages.concat tag_pages
     end
